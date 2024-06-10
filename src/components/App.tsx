970adb06aca58d09e5cb2e652e30236c82b04cf6
lxndrbukin <lxndrbukin@gmail.com>
2024-06-10 07:34:36 +0100

    Install and implement React Router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
-import { ChangeEvent, useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { ChangeEvent, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   AppDispatch,
   DataProps,
@@ -7,8 +7,8 @@ import {
   getCrypto,
   getHistory,
   changePair,
-} from "../store";
-import { socket } from "../socket";
+} from '../store';
+import { socket } from '../socket';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -19,9 +19,11 @@ import {
   Tooltip,
   Legend,
   Filler,
-} from "chart.js";
-import { Line } from "react-chartjs-2";
-import { formatData } from "../utils";
+} from 'chart.js';
+import { Outlet } from 'react-router-dom';
+
+import { Line } from 'react-chartjs-2';
+import { formatData } from '../utils';
 
 ChartJS.register(
   CategoryScale,
@@ -39,19 +41,19 @@ export default function App(): JSX.Element {
   const { currencies, historical, pair } = useSelector(
     (state: RootState): DataProps => state.data
   );
-  const [price, setPrice] = useState("0.00");
+  const [price, setPrice] = useState('0.00');
 
   useEffect(() => {
     dispatch(getCrypto());
-    socket.addEventListener("open", () => {
-      sendMessage("subscribe");
+    socket.addEventListener('open', () => {
+      sendMessage('subscribe');
     });
   }, [dispatch]);
 
   useEffect(() => {
-    socket.addEventListener("message", (e) => {
+    socket.addEventListener('message', (e) => {
       const data = JSON.parse(e.data);
-      if (data.type !== "ticker") {
+      if (data.type !== 'ticker') {
         return;
       }
       if (data.product_id === pair) {
@@ -65,16 +67,16 @@ export default function App(): JSX.Element {
   }, [dispatch, pair]);
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement>): void => {
-    sendMessage("unsubscribe");
+    sendMessage('unsubscribe');
     dispatch(changePair(e.target.value));
-    sendMessage("subscribe", e.target.value);
+    sendMessage('subscribe', e.target.value);
   };
 
   const sendMessage = (type: string, pairValue: string = pair): void => {
     const msg = {
       type,
       product_ids: [pairValue],
-      channels: ["ticker"],
+      channels: ['ticker'],
     };
     const jsonMsg = JSON.stringify(msg);
     socket.send(jsonMsg);
@@ -82,13 +84,14 @@ export default function App(): JSX.Element {
 
   return (
     <main>
-      <select onChange={handleChange} defaultValue={"BTC-USD"}>
-        <option value={"BTC-USD"}>BTC-USD</option>
-        <option value={"ETH-USD"}>ETH-USD</option>
-        <option value={"RNDR-USD"}>RNDR-USD</option>
+      <select onChange={handleChange} defaultValue={'BTC-USD'}>
+        <option value={'BTC-USD'}>BTC-USD</option>
+        <option value={'ETH-USD'}>ETH-USD</option>
+        <option value={'RNDR-USD'}>RNDR-USD</option>
       </select>
       {price}
       {/* <Line data={formatData((historical as Array<Array<number>>) || [])} /> */}
+      <Outlet />
     </main>
   );
 }
